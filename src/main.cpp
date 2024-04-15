1a3cd4eac7c113f7673fc227fba0b1caaaf84bb3
HODUCVU <hoducvu1234@gmail.com>
2024-04-15 22:04:17 +0700

    Modify platform to WROOM KIT

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 // #include <TaskScheduler.h>
 #include <FreeRTOSConfig.h>
-// #include <freertos/FreeRTOS.h>
-// #include "freertos/task.h"
 // Define PIN
 #define LED_1 2
 
@@ -13,7 +11,7 @@ void printS(void *pvParameter);
 
 void setup() {
 
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(LED_1, OUTPUT);
 
   xTaskCreate(&blink_task_LED1, "Blink led 1", 128,NULL,2,NULL);
@@ -25,17 +23,17 @@ void loop() {}
 void blink_task_LED1(void *pvParameter)
 {
     while(1) {
-        /* Blink off (output low) */
+        /* Blink off (output, low); */
         digitalWrite(LED_1, HIGH);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output high) */
+        /* Blink on (output, high);  */
         digitalWrite(LED_1, LOW);
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
 }
 void printS(void * pvParameter) {
   while(1) {
-    Serial.printf("Hello\n");
+    Serial.print("Hello\n");
     vTaskDelay(1500/portTICK_PERIOD_MS);
   }
 } 
