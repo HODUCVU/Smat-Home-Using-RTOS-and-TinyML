2828a9acc0c9b701708be1da79a1b782ab7ae61a
HODUCVU <hoducvu1234@gmail.com>
2024-04-15 22:50:17 +0700

    fixed FreeRTOS issue

--- src/main.cpp
+++ src/main.cpp
@@ -5,35 +5,60 @@
 #define LED_1 2
 
 // Initialization function
-void initPort();
-void blink_task_LED1(void *pvParameter);
-void printS(void *pvParameter);
-
-void setup() {
-
-  Serial.begin(9600);
-  pinMode(LED_1, OUTPUT);
-
-  xTaskCreate(&blink_task_LED1, "Blink led 1", 128,NULL,2,NULL);
-  xTaskCreate(&printS, "Hello", 128, NULL, 1, NULL);
-}
-
-void loop() {}
+// void initPort();
 
 void blink_task_LED1(void *pvParameter)
 {
-    while(1) {
-        /* Blink off (output, low); */
-        digitalWrite(LED_1, HIGH);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        /* Blink on (output, high);  */
-        digitalWrite(LED_1, LOW);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+  while(1) {
+      /* Blink off (output, low); */
+      digitalWrite(LED_1, HIGH);
+      vTaskDelay(1000 / portTICK_PERIOD_MS);
+      /* Blink on (output, high);  */
+      digitalWrite(LED_1, LOW);
+      vTaskDelay(3000 / portTICK_PERIOD_MS);
+  }
 }
 void printS(void * pvParameter) {
   while(1) {
     Serial.print("Hello\n");
-    vTaskDelay(1500/portTICK_PERIOD_MS);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 } 
+void printTest(void *pvParameter){
+  while (1)
+  {
+    Serial.print("Test\n");
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+  }
+  
+}
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED_1, OUTPUT);
+  xTaskCreate(blink_task_LED1, "Blink led 1", 1024, NULL, 1, NULL);
+  xTaskCreate(printS, "Hello", 1024, NULL, 1, NULL);
+  xTaskCreate(printTest, "Test", 1024, NULL, 2, NULL);
+}
+
+void loop() {
+  // Serial.print("ttt");
+  // delay(1000);
+}
+
+// void blink_task_LED1(void *pvParameter)
+// {
+//   while(1) {
+//       /* Blink off (output, low); */
+//       digitalWrite(LED_1, HIGH);
+//       vTaskDelay(1000 / portTICK_PERIOD_MS);
+//       /* Blink on (output, high);  */
+//       digitalWrite(LED_1, LOW);
+//       vTaskDelay(1000 / portTICK_PERIOD_MS);
+//   }
+// }
+// void printS(void * pvParameter) {
+//   while(1) {
+//     Serial.print("Hello\n");
+//     vTaskDelay(1000/portTICK_PERIOD_MS);
+//   }
+// } 
