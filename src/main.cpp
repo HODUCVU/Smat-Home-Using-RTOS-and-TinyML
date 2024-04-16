0cda1799f067e8c20860f30dfa22dfbcfbbd2ad8
HODUCVU <hoducvu1234@gmail.com>
2024-04-16 23:41:14 +0700

    Build Framework

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <MQ135.h>
 // Define PINi
 // Output pin
 #define LIGHTPIN 25
@@ -10,10 +11,10 @@
 // Input pin
 #define DHTPIN 12 
 #define DHTTYPE DHT22
-#define MQ2PIN 13
+#define MQ135PIN 13
 // Initialization global values
 DHT_Unified dht(DHTPIN, DHTTYPE);
-
+MQ135 mq135_sensor(MQ135PIN);
 // Queues for FreeRTOSConfig
 static QueueHandle_t tempReading;
 static QueueHandle_t smokingReading;
@@ -32,10 +33,9 @@ void initValue(bool permissionInit= false) {
   if (permissionInit) {
     fanStatus = false;
     lightStatus = false;
-    pinMode(LIGHTPIN, OUTPUT);
     dht.begin();
-    tempReading = xQueueCreate(10, sizeof(int));
-    smokingReading = xQueueCreate(10, sizeof(int));
+    tempReading = xQueueCreate(10, sizeof(float));
+    smokingReading = xQueueCreate(10, sizeof(float));
   } else {
     printf("Initialization values are not permission");
   }
@@ -79,35 +79,64 @@ void setup() {
   initValue(true);
   inforDHT();
   definingPinMode(true);
-  /* xTaskCreate(blink_task_LED1, "Blink led 1", 1024, NULL, 1, NULL); */
-  /* xTaskCreate(printS, "Hello", 1024, NULL, 1, NULL); */
-  /* xTaskCreate(printTest, "Test", 1024, NULL, 2, NULL); */
+
+  // Create Task 
+  xTaskCreate(taskTempRead, "Reading Temperature", 2048, NULL, 1, NULL);
+  xTaskCreate(taskSmokeDetect, "Smoking Detect", 2048, NULL, 1, NULL);
+  /* xTaskCreate(taskControlFan, "Controller Fan", 1024, NULL, 1, NULL); */
+  /* xTaskCreate(taskControlLight, "Controller Light", 1024, NULL, 1, NULL); */
 }
 
 void loop() {}
-/*
-void blink_task_LED1(void *pvParameter)
-{
-  while(1) {
-      // Blink off (output, low); 
-      digitalWrite(LED_1, HIGH);
-      vTaskDelay(1000 / portTICK_PERIOD_MS);
-      // Blink on (output, high); 
-      digitalWrite(LED_1, LOW);
-      vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+void taskTempRead(void *pvParameter) {
+  sensors_event_t event;
+  while (true) {
+    // Read Temperature
+    dht.temperature().getEvent(&event);
+    if(isnan(event.temperature)) {
+      Serial.println(F("Error reading temperature"));
+      // return 0.0; -> Queues give
+    }
+    Serial.print(F("Temperature: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+    // return float(event.temperature); -> Queues give
+    vTaskDelay(500/portTICK_PERIOD_MS);
   }
 }
-void printS(void * pvParameter) {
-  while(1) {
-    Serial.print("Hello\n");
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-  }
-} 
-void printTest(void *pvParameter){
-  while (1)
-  {
-    Serial.print("Test\n");
-    vTaskDelay(1000/portTICK_PERIOD_MS);
+void taskSmokeDetect(void *pvParameter) {
+  // Read humidity
+  sensors_event_t event;
+  while(true) {
+    dht.humidity().getEvent(&event);
+    if(isnan(event.relative_humidity)) {
+      Serial.println(F("Error reading humidity"));
+      //return 0.0; -> Queues give
+    }
+    float humidity = event.relative_humidity;
+    // temperature = tempReading(); -> Solve later -> Queues take
+    // Use temporary temperature
+    float temperature = 25;
+    float correctedRZero = mq135_sensor.getCorrectedRZero(temperature, humidity);
+    float resistance = mq135_sensor.getResistance();
+    float correctedPPM = mq135_sensor.getCorrectedPPM(temperature, humidity);
+    // Queues Give values (I think we will use ppm for detect smoke)
+    Serial.println("Smoking Detect: ");
+    Serial.print("Rzero: ");
+    Serial.println(correctedRZero);
+    Serial.print("Resistance: ");
+    Serial.println(resistance);
+    Serial.print("PPM: ");
+    Serial.println(correctedPPM);
+    vTaskDelay(500/portTICK_PERIOD_MS);
   }
 }
-*/
+void taskControlLight(void *pvParameter) {
+  // When temperature is cool -> On else Off
+  // When voice "On" and "Off"
+}
+void taskControlFan(void *pvParameter) {
+  // Air is polluted or temperature is hot -> On else Off
+  // When voice "Activate" and "Deactivate"
+}
