4df39f57f47d3e28c7452f3a4a41956b4d50e579
HODUCVU <hoducvu1234@gmail.com>
2024-05-29 21:19:44 +0700

    Opt completed

--- src/main.cpp
+++ src/main.cpp
@@ -14,24 +14,6 @@
 #include "CommandDetector.h"
 #include "CommandProcessor.h"
 
-// // Output pin
-// #ifndef LIGHTPIN 
-// #define LIGHTPIN 5
-// #endif
-// #ifndef FANPIN 
-// #define FANPIN 4
-// #endif
-// // Input pin
-// #ifndef DHTPIN 
-// #define DHTPIN 26
-// #endif
-// #ifndef DHTTYPE 
-// #define DHTTYPE DHT22
-// #endif
-// #ifndef MQ135PIN
-// #define MQ135PIN 25
-// #endif
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
@@ -64,19 +46,15 @@ protected:
   int _pin;
   QueueHandle_t _queuesReading = NULL;
 public:
-  
+
   Sensors() {}
   Sensors(int pin) {
-    // Serial.println("Initialization Sensor");
     _pin = pin;
     _queuesReading = xQueueCreate(10, sizeof(float));  
-    // Serial.println("Complete init Sensor");
   }
   Sensors(int pin, int sizeQueues) {
-    // Serial.print("Initialization Sensor");
     _pin = pin;
     _queuesReading = xQueueCreate(sizeQueues, sizeof(float));  
-    // Serial.println("Complete init Sensor");
   }
   ~Sensors() {
     // print delete Sensors
@@ -85,9 +63,9 @@ public:
 
   void sendQueuesHandle(float value) {
     try {
-    if(_queuesReading != NULL) {
-      xQueueSend(_queuesReading, (void*)&value,(TickType_t) 0);
-    }
+      if(_queuesReading != NULL) {
+        xQueueSend(_queuesReading, (void*)&value,(TickType_t) 0);
+      }
     } catch (std::exception &e) {
       Serial.print("Error in 95: ");
       Serial.print(e.what());
@@ -103,14 +81,10 @@ private:
 public:
   Temperature(){}
   Temperature(int pin) : Sensors(pin) {
-    // Serial.println("Initialization Temperature");
     dht.begin();
-    // Serial.println("Complete Temperature");
   }
   Temperature(int pin, int sizeQueues) : Sensors(pin, sizeQueues) {
-    // Serial.println("Initialization Temperature");
     dht.begin();
-    // Serial.println("Complete Temperature");
   }
   float getTemp() {
     return _temp;
@@ -122,7 +96,6 @@ public:
         Serial.print("Temperature: ");
         Serial.println(_temp);
         if(!isnan(_temp)) {
-          // Serial.println("queue-temp");
           sendQueuesHandle(_temp);
         }
         vTaskDelay(500/portTICK_PERIOD_MS);
@@ -143,10 +116,8 @@ private:
 public:
   Smoking() {}
   Smoking(int pin) : Sensors(pin) {
-    // Serial.println("Initialization Smoking");
   }
   Smoking(int pin, int sizeQueues) : Sensors(pin, sizeQueues) {
-    // Serial.println("Initialization Smoking");
   }
   ~Smoking() {
     // delete queues
@@ -161,7 +132,6 @@ public:
         Serial.print("Smoking: ");
         Serial.println(_smoking);
         if(!isnan(_smoking)) {
-          // Serial.println("queue-smoking");
           sendQueuesHandle(_smoking);
         }
         vTaskDelay(500/portTICK_PERIOD_MS);
@@ -194,7 +164,7 @@ public:
   void virtual taskHandleObjectTemp(void *pvParameter) = 0;
   void virtual taskHandleObjectSmoking(void *pvParameter) = 0;
   void virtual detectVoice() = 0;
-  void virtual controllHandle() = 0;
+  // void virtual controllHandle() = 0;
 };
 class Light : public Objects {
 private:
@@ -203,8 +173,6 @@ private:
 public:
   Light() {}
   Light(int pin) : Objects(pin) {
-    // setup pin mode and so on.
-    // Serial.println("Initialization Light");
   }
   ~Light() {
     // free QueueHandle_t and so on.
@@ -214,13 +182,11 @@ public:
     try {
       while(true) {
         if (tempOB == NULL) {
-                Serial.println("Error: globalTempSensor is NULL");
-                vTaskDelete(NULL);
-                return;
+          Serial.println("Error: globalTempSensor is NULL");
+          vTaskDelete(NULL);
+          return;
         }
         xQueueReceive(tempOB->getQueuesHandle(),(void *)&_tempForLight,portMAX_DELAY);
-        // Serial.print("Value _tempForLight: ");
-        // Serial.println(_tempForLight);
         // temp < 25
         if(_tempForLight <= 31 && !_stateForTemp) {
           digitalWrite(_pin, HIGH);
@@ -242,13 +208,11 @@ public:
     try {
       while(true) {
         if (smokingOB == NULL) {
-                Serial.println("Error: globalSmokingSensor is NULL");
-                vTaskDelete(NULL);
-                return;
+          Serial.println("Error: globalSmokingSensor is NULL");
+          vTaskDelete(NULL);
+          return;
         }
         xQueueReceive(smokingOB->getQueuesHandle(),(void *)&_smokingForLight,portMAX_DELAY);
-        // Serial.print("Value _smokingForLight: ");
-        // Serial.println(_smokingForLight);
         if(_smokingForLight >= 1700 && !_stateForSmoking) {
           digitalWrite(_pin, !digitalRead(_pin));
           Serial.println("Light is blinking");
@@ -265,7 +229,7 @@ public:
       Serial.println(e.what());
     }
   }
-  void detectVoice() override{}
+  // void detectVoice() override{}
   void controllHandle() override {}
 
 };
@@ -276,8 +240,6 @@ private:
 public:
   Fan(){}
   Fan(int pin) : Objects(pin) {
-    // setup pin mode and so on.
-    //  Serial.println("Initialization Fan");
   }
   ~Fan() {
     // free QueueHandle_t and so on.
@@ -287,13 +249,11 @@ public:
     try {
       while(true) {
         if (tempOB == NULL) {
-                Serial.println("Error: globalTempSensor is NULL");
-                vTaskDelete(NULL);
-                return;
+          Serial.println("Error: globalTempSensor is NULL");
+          vTaskDelete(NULL);
+          return;
         }
         xQueueReceive(tempOB->getQueuesHandle(),(void *)&_tempForFan,portMAX_DELAY);
-        // Serial.print("Value _tempForFan: ");
-        // Serial.println(_tempForFan);
         // temp < 25
         if(_tempForFan >= 35 && !_stateForTemp) {
           digitalWrite(_pin, HIGH);
@@ -315,13 +275,11 @@ public:
     try {
       while(true) {
         if (smokingOB == NULL) {
-                Serial.println("Error: globalSmokingSensor is NULL");
-                vTaskDelete(NULL);
-                return;
+          Serial.println("Error: globalSmokingSensor is NULL");
+          vTaskDelete(NULL);
+          return;
         }
         xQueueReceive(smokingOB->getQueuesHandle(),(void *)&_smokingForFan,portMAX_DELAY);
-        // Serial.print("Value _smokingForFan: ");
-        // Serial.println(_smokingForFan);
         if(_smokingForFan >= 1700 && !_stateForSmoking) {
           digitalWrite(_pin, HIGH);
           Serial.println("Fan is on");
@@ -338,126 +296,126 @@ public:
       Serial.println(e.what());
     }
   }
-  void detectVoice() override {}
+  // void detectVoice() override {}
   void controllHandle() override {}
 };
 
 Light *lightOB = NULL;
 Fan *fanOB = NULL;
 static void taskTemp(void *pvParameter) {
-    try {
-      if (tempOB == NULL) {
-                Serial.println("Error: globalTempSensor is NULL");
-                vTaskDelete(NULL);
-                return;
-      }
-    tempOB->taskHandleSensor(pvParameter);
-    } catch (std::exception &e) {
-      Serial.print("Error in 251: ");
-      Serial.print(e.what());
+  try {
+    if (tempOB == NULL) {
+      Serial.println("Error: globalTempSensor is NULL");
+      vTaskDelete(NULL);
+      return;
     }
+    tempOB->taskHandleSensor(pvParameter);
+  } catch (std::exception &e) {
+    Serial.print("Error in 251: ");
+    Serial.print(e.what());
+  }
 }
 static void taskSmoking(void *pvParameter) {
-    try {
-      if (smokingOB == NULL) {
-                Serial.println("Error: globalSmokingSensor is NULL");
-                vTaskDelete(NULL);
-                return;
-      }
-    smokingOB->taskHandleSensor(pvParameter);
-    } catch (std::exception &e) {
-      Serial.print("Error in 267: ");
-      Serial.print(e.what());
+  try {
+    if (smokingOB == NULL) {
+      Serial.println("Error: globalSmokingSensor is NULL");
+      vTaskDelete(NULL);
+      return;
     }
+    smokingOB->taskHandleSensor(pvParameter);
+  } catch (std::exception &e) {
+    Serial.print("Error in 267: ");
+    Serial.print(e.what());
+  }
 }
 static void taskLightForTemp(void *pvParameter) {
-    try {
-      if (lightOB == NULL) {
-                Serial.println("Error: globalLightSensor is NULL");
-                vTaskDelete(lightOB->_handle);
-                return;
-      }
-    lightOB->taskHandleObjectTemp(pvParameter);
-    } catch (std::exception &e) {
-      Serial.print("Error in 300: ");
-      Serial.print(e.what());
+  try {
+    if (lightOB == NULL) {
+      Serial.println("Error: globalLightSensor is NULL");
+      vTaskDelete(lightOB->_handle);
+      return;
     }
+    lightOB->taskHandleObjectTemp(pvParameter);
+  } catch (std::exception &e) {
+    Serial.print("Error in 300: ");
+    Serial.print(e.what());
+  }
 }
 static void taskLightForSmoking(void *pvParameter) {
-    try {
-      if (lightOB == NULL) {
-                Serial.println("Error: globalLightSensor is NULL");
-                vTaskDelete(lightOB->_handle);
-                return;
-      }
-    lightOB->taskHandleObjectSmoking(pvParameter);
-    } catch (std::exception &e) {
-      Serial.print("Error in 342: ");
-      Serial.print(e.what());
+  try {
+    if (lightOB == NULL) {
+      Serial.println("Error: globalLightSensor is NULL");
+      vTaskDelete(lightOB->_handle);
+      return;
     }
+    lightOB->taskHandleObjectSmoking(pvParameter);
+  } catch (std::exception &e) {
+    Serial.print("Error in 342: ");
+    Serial.print(e.what());
+  }
 }
 static void taskFanForTemp(void *pvParameter) {
-    try {
-      if (fanOB == NULL) {
-                Serial.println("Error: globalFanSensor is NULL");
-                vTaskDelete(fanOB->_handle);
-                return;
-      }
-    fanOB->taskHandleObjectTemp(pvParameter);
-    } catch (std::exception &e) {
-      Serial.print("Error in 300: ");
-      Serial.print(e.what());
+  try {
+    if (fanOB == NULL) {
+      Serial.println("Error: globalFanSensor is NULL");
+      vTaskDelete(fanOB->_handle);
+      return;
     }
+    fanOB->taskHandleObjectTemp(pvParameter);
+  } catch (std::exception &e) {
+    Serial.print("Error in 300: ");
+    Serial.print(e.what());
+  }
 }
 static void taskFanForSmoking(void *pvParameter) {
-    try {
-      if (fanOB == NULL) {
-                Serial.println("Error: globalFanSensor is NULL");
-                vTaskDelete(fanOB->_handle);
-                return;
-      }
-    fanOB->taskHandleObjectSmoking(pvParameter);
-    } catch (std::exception &e) {
-      Serial.print("Error in 342: ");
-      Serial.print(e.what());
+  try {
+    if (fanOB == NULL) {
+      Serial.println("Error: globalFanSensor is NULL");
+      vTaskDelete(fanOB->_handle);
+      return;
     }
+    fanOB->taskHandleObjectSmoking(pvParameter);
+  } catch (std::exception &e) {
+    Serial.print("Error in 342: ");
+    Serial.print(e.what());
+  }
 }
 
 //  =================================
 // i2s config for using the internal ADC
 i2s_config_t adcI2SConfig = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
-    .sample_rate = 16000,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
-    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-    .communication_format = I2S_COMM_FORMAT_I2S_LSB,
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 4,
-    .dma_buf_len = 64,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0};
+  .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
+  .sample_rate = 16000,
+  .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+  .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+  .communication_format = I2S_COMM_FORMAT_I2S_LSB,
+  .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+  .dma_buf_count = 4,
+  .dma_buf_len = 64,
+  .use_apll = false,
+  .tx_desc_auto_clear = false,
+  .fixed_mclk = 0};
 
 // i2s config for reading from both channels of I2S
 i2s_config_t i2sMemsConfigBothChannels = {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
-    .sample_rate = 16000,
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
-    .channel_format = I2S_MIC_CHANNEL,
-    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
-    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
-    .dma_buf_count = 4,
-    .dma_buf_len = 64,
-    .use_apll = false,
-    .tx_desc_auto_clear = false,
-    .fixed_mclk = 0};
+  .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+  .sample_rate = 16000,
+  .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+  .channel_format = I2S_MIC_CHANNEL,
+  .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+  .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+  .dma_buf_count = 4,
+  .dma_buf_len = 64,
+  .use_apll = false,
+  .tx_desc_auto_clear = false,
+  .fixed_mclk = 0};
 
 // i2s microphone pins
 i2s_pin_config_t i2s_mic_pins = {
-    .bck_io_num = I2S_MIC_SERIAL_CLOCK,
-    .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
-    .data_out_num = I2S_PIN_NO_CHANGE,
-    .data_in_num = I2S_MIC_SERIAL_DATA};
+  .bck_io_num = I2S_MIC_SERIAL_CLOCK,
+  .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
+  .data_out_num = I2S_PIN_NO_CHANGE,
+  .data_in_num = I2S_MIC_SERIAL_DATA};
 
 // This task does all the heavy lifting for our application
 void voiceTask(void *param)
@@ -482,51 +440,50 @@ void setup() {
   delay(1000);
   Serial.println("Starting up");
   try {
-  tempOB = new Temperature(DHTPIN);
-  smokingOB = new Smoking(MQ135PIN);
-  lightOB = new Light(LIGHTPIN);
-  fanOB = new Fan(FANPIN);
+    tempOB = new Temperature(DHTPIN);
+    smokingOB = new Smoking(MQ135PIN);
+    lightOB = new Light(LIGHTPIN);
+    fanOB = new Fan(FANPIN);
     // make sure we don't get killed for our long running tasks
-  esp_task_wdt_init(10, false);
+    esp_task_wdt_init(10, false);
     // start up the I2S input (from either an I2S microphone or Analogue microphone via the ADC)
 #ifdef USE_I2S_MIC_INPUT
-  // Direct i2s input from INMP441 or the SPH0645
-  I2SSampler *i2s_sampler = new I2SMicSampler(i2s_mic_pins, false);
+    // Direct i2s input from INMP441 or the SPH0645
+    I2SSampler *i2s_sampler = new I2SMicSampler(i2s_mic_pins, false);
 #else
-  // Use the internal ADC
-  I2SSampler *i2s_sampler = new ADCSampler(ADC_UNIT_1, ADC_MIC_CHANNEL);
+    // Use the internal ADC
+    I2SSampler *i2s_sampler = new ADCSampler(ADC_UNIT_1, ADC_MIC_CHANNEL);
 #endif
-  // the command processor
-  CommandProcessor *command_processor = new CommandProcessor();
+    // the command processor
+    CommandProcessor *command_processor = new CommandProcessor();
 
-  // create our application
-  CommandDetector *commandDetector = new CommandDetector(i2s_sampler, command_processor);
+    // create our application
+    CommandDetector *commandDetector = new CommandDetector(i2s_sampler, command_processor);
 
-  // set up the i2s sample writer task
-  TaskHandle_t applicationTaskHandle;
-  // xTaskCreatePinnedToCore(voiceTask, "Command Detect", 8192, commandDetector, 1, &applicationTaskHandle, 0)
-  if (tempOB->getQueuesHandle() == NULL || smokingOB->getQueuesHandle() == NULL) {
-    Serial.println("queue NULL");
-  } else {
-    xTaskCreatePinnedToCore(voiceTask, "Command Detect", 8192, commandDetector, 4, &applicationTaskHandle, 0);
-    xTaskCreate(taskTemp, "Reading temperature", 2048, NULL, 3, NULL);
-    xTaskCreate(taskSmoking, "Smoking Detect", 2048, NULL, 3, NULL); 
-    xTaskCreate(taskLightForTemp, "Controller Light temp", 1024, NULL, 2, &(lightOB->_handle)); 
-    xTaskCreate(taskLightForSmoking, "Controller Light smoking", 1024, NULL, 2, &(lightOB->_handle)); 
-    xTaskCreate(taskFanForTemp, "Controller Fan temp", 1024, NULL, 2, &(fanOB->_handle)); 
-    xTaskCreate(taskFanForSmoking, "Controller Fan smoking", 1024, NULL, 2, &(fanOB->_handle)); 
-    // xTaskCreate(voiceTask, "Command Detect", 8192, commandDetector, 4, &applicationTaskHandle);
-  }
+    // set up the i2s sample writer task
+    TaskHandle_t applicationTaskHandle;
+    if (tempOB->getQueuesHandle() == NULL || smokingOB->getQueuesHandle() == NULL) {
+      Serial.println("queue NULL");
+    } else {
+      xTaskCreatePinnedToCore(voiceTask, "Command Detect", 8192, commandDetector, 4, &applicationTaskHandle, 0);
+      xTaskCreate(taskTemp, "Reading temperature", 2048, NULL, 3, NULL);
+      xTaskCreate(taskSmoking, "Smoking Detect", 2048, NULL, 3, NULL); 
+      xTaskCreate(taskLightForTemp, "Controller Light temp", 1024, NULL, 2, &(lightOB->_handle)); 
+      xTaskCreate(taskLightForSmoking, "Controller Light smoking", 1024, NULL, 2, &(lightOB->_handle)); 
+      xTaskCreate(taskFanForTemp, "Controller Fan temp", 1024, NULL, 2, &(fanOB->_handle)); 
+      xTaskCreate(taskFanForSmoking, "Controller Fan smoking", 1024, NULL, 2, &(fanOB->_handle)); 
+      // xTaskCreate(voiceTask, "Command Detect", 8192, commandDetector, 4, &applicationTaskHandle);
+    }
 
-  // start sampling from i2s device - use I2S_NUM_0 as that's the one that supports the internal ADC
+    // start sampling from i2s device - use I2S_NUM_0 as that's the one that supports the internal ADC
 #ifdef USE_I2S_MIC_INPUT
-  i2s_sampler->start(I2S_NUM_0, i2sMemsConfigBothChannels, applicationTaskHandle);
+    i2s_sampler->start(I2S_NUM_0, i2sMemsConfigBothChannels, applicationTaskHandle);
 #else
-  i2s_sampler->start(I2S_NUM_0, adcI2SConfig, applicationTaskHandle);
+    i2s_sampler->start(I2S_NUM_0, adcI2SConfig, applicationTaskHandle);
 #endif
   } catch (std::exception &e) {
-      Serial.println("Error in 232: ");
-      Serial.println(e.what());
+    Serial.println("Error in 232: ");
+    Serial.println(e.what());
   }
 
 }
